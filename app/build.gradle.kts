730bc9ee03294b051534951d980523510d94b187
cozenom <david.p646@gmail.com>
2024-10-10 21:02:49 -0700

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.calendarapp"
+        applicationId = "com.example.calendarapp.stable"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
