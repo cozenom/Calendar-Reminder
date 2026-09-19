bbfca797beaa7de524e38c7f4b57beaa0185f09d
David <david.p646@gmail.com>
2026-09-18 22:52:57 -0700

    Auto-capitalize first letter in reminder title and notes

--- app/src/main/java/com/davidp/simpleweeklyreminders/ui/form/ReminderForm.kt
+++ app/src/main/java/com/davidp/simpleweeklyreminders/ui/form/ReminderForm.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -43,6 +44,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.davidp.simpleweeklyreminders.data.model.DEFAULT_ICON_KEY
@@ -166,6 +168,7 @@ private fun ReminderForm(
                     }
                 },
                 singleLine = true,
+                keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
                 modifier = Modifier.weight(1f)
             )
         }
@@ -310,6 +313,7 @@ private fun ReminderForm(
             onValueChange = { notes = it },
             placeholder = { Text("Optional") },
             shape = MaterialTheme.appShapes.medium,
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
             modifier = Modifier.fillMaxWidth(),
             minLines = 2
         )
